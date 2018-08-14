83f8b18479564e4b90dab3b64e4263eca68b98e8
wjddl <wjddl@192.168.219.103>
2018-08-14 15:37:52 +0900

    되나

--- cloudFunding/src/main/java/com/tikitaka/cloudFunding/project/controller/ProjectController.java
+++ cloudFunding/src/main/java/com/tikitaka/cloudFunding/project/controller/ProjectController.java
@@ -20,19 +20,9 @@ public class ProjectController {
 
 	@Autowired
 	ProjectService projectService;
-<<<<<<< HEAD
-=======
-
-	
-
->>>>>>> refs/heads/master
 	@Autowired
 	CommunityService cService;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> refs/heads/master
 	@RequestMapping("projectList.do")
 	public String projectList(){
 		return "project/projectList";
